a855c53f66004b723940ece802f9186816cc9656
andre.silva <andre.silva@marimex.com.br>
2024-05-24 10:04:20 -0300

    botões de Navegação com Stack Navigation

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,9 +1,24 @@
 import {NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from '../paginas/Home/index.js';
+import Login from '../paginas/Login/index.js';
+import Cadastro from "../paginas/Cadastro/index.js";
+
+const Stack = createStackNavigator();
 
 export default function Navigation() {
     return(
         <NavigationContainer>
-
+            <Stack.Navigator 
+            initialRouteName="Home"
+            screenOptions={{
+                headerTitle: '',
+                headerShown: false
+            }}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="Cadastro" component={Cadastro}/>
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
