4d95fe233fb709125cc8b70ffc377fc9d1fbb05e
andre.silva <andre.silva@marimex.com.br>
2024-05-24 10:34:35 -0300

    Tab Navigation, Alinhamento e Múltiplas Navegações

--- src/paginas/Mensagem/index.js
+++ src/paginas/Mensagem/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import PaginaBase from "../PaginaBase";
 
--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,10 +1,39 @@
 import {NavigationContainer} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from '../paginas/Home/index.js';
-import Login from '../paginas/Login/index.js';
-import Cadastro from "../paginas/Cadastro/index.js";
+import Login from '../paginas/Login';
+import Cadastro from "../paginas/Cadastro";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ListaPets from '../paginas/ListaPets';
+import Mensagem from '../paginas/Mensagem';
+import { Image } from "react-native";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoutes(){
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false
+            }}>
+            <Tab.Screen name="Lista de Pets" component={ListaPets}
+                options={{
+                    tabBarIcon: () => (
+                        <Image source={require('../assets/pets-green.png')}  style={{ width: 24, height: 24 }} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Mensagens" component={Mensagem}
+                options={{
+                    tabBarIcon: () => (
+                        <Image source={require('../assets/mensagens.png')} style={{ width: 24, height: 24  }}/>
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
 
 export default function Navigation() {
     return(
@@ -18,6 +47,7 @@ export default function Navigation() {
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Login" component={Login}/>
                 <Stack.Screen name="Cadastro" component={Cadastro}/>
+                <Stack.Screen name="Tab" component={TabRoutes} />
             </Stack.Navigator>
         </NavigationContainer>
     )
